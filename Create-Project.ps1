c51b86f1f3c1749aeb06121d3e3e2ae793b31a9f
David Stevenson <dstevenson@dynamiccontrols.com>
2017-07-24 20:09:04 +1200

    added powershell cmdlet and license and updated the readme

---
+++ Create-Project.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+[parameter(Mandatory=$true)][alias("l")]$ProjectLanguage,
+[parameter(Mandatory=$true)][alias("n")]$ProjectName
+)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+git archive --format=zip --prefix=$ProjectName/ proj/$ProjectLanguage -o __tmp__.zip
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host git failed, possible language are:
+    foreach ($a in git branch --list proj/*)
+    {
+        Write-Host   - $a.Substring(7)
+    }
+    Break
+}
+
+Unzip $pwd\__tmp__.zip $pwd\
+Remove-Item -Path __tmp__.zip
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-templates
+## Project Templates
+
+Boiler plate projects setup and settings
+
+
+
+#### Details
+
+The idea here is to provide a script in the `master` branch that enables the creation of new projects setup with all the required boiler plate code
+
+Current syntax for this (using PowerShell)
+`$ Create-Project -ProjectName TestProject -ProjectLanguage cpp` 
+or
+`$ Create-Project -n TestProject -l cpp`
+
+Each project boiler plate code is kept in a branch under the `proj` namespace, e.g. `proj/cpp`
+
+
+
+#### License
+
+This code is licensed under the MIT License
